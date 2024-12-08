de049b740ac115b4a6fc1c0f40ec2250024056f7
Martin Barker <martinbarker99@gmail.com>
2024-12-07 18:27:33 -0800

    execa_fail_test

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ const path = require('path');
 const serve = require('electron-serve');
 const loadURL = serve({ directory: 'build' });
 const { autoUpdater } = require('electron-updater');
-const express = require('express');
-const http = require('http');
-const fs = require('fs');
-const { exec } = require('child_process');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -28,194 +24,62 @@ function createWindow() {
     backgroundColor: '#FFF',
     webPreferences: {
       // Set a Content Security Policy for the renderer process
+      // This example allows scripts and styles only from the same origin
+      // You should customize this policy to fit your app's needs
       contentSecurityPolicy: "default-src 'self'; script-src 'self'; style-src 'self';",
       enableRemoteModule: true,
       nodeIntegration: true,
-      contextIsolation: false,
+      contextIsolation: false
     },
-    show: false,
+    // Use this in development mode.
+    icon: isDev() ? path.join(process.cwd(), 'public/logo512.png') : path.join(__dirname, 'build/logo512.png'),
+    // Use this in production mode.
+    // icon: path.join(__dirname, 'build/logo512.png'),
+    show: false
   });
 
-  // Load the URL depending on the environment (development vs production)
+  // This block of code is intended for development purpose only.
+  // Delete this entire block of code when you are ready to package the application.
   if (isDev()) {
-    // Load localhost if in development mode
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL('http://localhost:3000/');
   } else {
-    // Load the index.html from the build folder in production mode
-    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+    loadURL(mainWindow);
   }
 
-  // Open DevTools for development mode (optional)
-  if (isDev()) {
-    process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
-    mainWindow.webContents.openDevTools();
-  }
+  // Uncomment the following line of code when app is ready to be packaged.
+  // loadURL(mainWindow);
 
-  mainWindow.webContents.on('did-fail-load', (event, errorCode, errorDescription) => {
-    console.error('Failed to load:', errorDescription);
-  });
+  // Open the DevTools and also disable Electron Security Warning.
+  // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
+  // mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is ready to be shown
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-    
-    // Only check for updates in production
-    if (!isDev()) {
-      autoUpdater.checkForUpdatesAndNotify();
-    }
-  });
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    mainWindow = null;
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  });
+
+  // Emitted when the window is ready to be shown
+  // This helps in showing the window gracefully.
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    autoUpdater.checkForUpdatesAndNotify();
   });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  stopServer();
-  if (process.platform !== 'darwin') app.quit();
+  stopServer()
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
 });
-
-app.on('activate', function () {
-  if (mainWindow === null) createWindow();
-});
-
-// App version update functions
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-});
-
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
-
-if (!isDev()) {
-  autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-
-  autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-  });
-}
-
-// App window controls functions
-
-// Minimize window event
-ipcMain.on('minimize-window', function () {
-  if (mainWindow && mainWindow.minimizable) {
-    mainWindow.minimize();
-  }
-});
-
-// Maximize window event
-ipcMain.on('maximize-window', function () {
-  if (mainWindow && mainWindow.maximizable) {
-    mainWindow.maximize();
-  }
-});
-
-// Unmaximize window event
-ipcMain.on('unmaximize-window', function () {
-  if (mainWindow) {
-    mainWindow.unmaximize();
-  }
-});
-
-// Toggle maximize/unmaximize event
-ipcMain.on('max-unmax-window', function () {
-  if (mainWindow) {
-    if (mainWindow.isMaximized()) {
-      mainWindow.unmaximize();
-    } else {
-      mainWindow.maximize();
-    }
-  }
-});
-
-// Close window event
-ipcMain.on('close-window', function () {
-  if (mainWindow) {
-    mainWindow.close();
-    app.quit(); // Ensure the app quits completely
-  }
-});
-
-ipcMain.on('render-ffmpeg-video', (event, filePaths) => {
-  const os = require('os');
-  const path = require('path');
-  const platform = os.platform();
-  const isWindows = platform === 'win32';
-
-  // Helper function to get FFmpeg/FFprobe paths
-  function getFfPath(cmd) {
-
-    // Set exe name if on Windows, otherwise use the command name
-    const exeName = isWindows ? `${cmd}.exe` : cmd;
-
-    if (app.isPackaged) {
-      return path.join(process.resourcesPath, exeName);
-    }
-
-    // Local development
-    const components = ['ffmpeg', `${platform}-x64`];
-    if (isWindows || platform === 'linux') components.push('lib');
-    components.push(exeName);
-    return path.join(...components);
-  }
-
-  const getFfprobePath = () => getFfPath('ffprobe');
-  const getFfmpegPath = () => getFfPath('ffmpeg');
-
-  // Get FFmpeg path
-  const ffmpegPath = getFfmpegPath();
-  console.log('ffmpegPath=', ffmpegPath)
-
-  // Output file path (e.g., Desktop/output.mp4)
-  const outputFilePath = path.join(app.getPath('desktop'), 'output.mp4');
-
-  // FFmpeg arguments to concatenate files
-  const ffmpegCmdArgs = ['-h'];
-
-  // Construct FFmpeg command
-  const ffmpegCommand = `${ffmpegPath} ${ffmpegCmdArgs.join(' ')}`;
-
-  // Execute FFmpeg command
-  exec(ffmpegCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error('Error running FFmpeg command:', error);
-      event.reply('render-ffmpeg-video-error', error.message);
-      return;
-    }
-
-    console.log('FFmpeg command executed successfully:', stdout || stderr);
-    event.reply('render-ffmpeg-video-success', outputFilePath);
-  });
-});
-
-
-// Server start and stop functions
-function startServer(port) {
-  const expressApp = express();
-  expressApp.get('/', (req, res) => {
-    res.send('API is running');
-  });
-
-  server = http.createServer(expressApp);
-  server.listen(port, () => {
-    console.log(`API server listening on port ${port}`);
-  });
-}
-
-function stopServer() {
-  if (server) {
-    server.close(() => {
-      console.log('API server stopped.');
-    });
-  }
-}
--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Frame from './Frame/Frame';
-import FfmpegTest from './FfmpegTest/FfmpegTest'
-import { runFfmpegProcess } from './Ffmpeg'; 
+const execa = window.require('execa');
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={ <Frame> <FfmpegTest/> </Frame> } />
-      </Routes>
-    </Router>
+      <h1 style={{ color: 'black' }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </h1>
     </>
   );
 }
